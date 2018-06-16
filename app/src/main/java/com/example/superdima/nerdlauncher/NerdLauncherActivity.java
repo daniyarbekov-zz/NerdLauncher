487598d0638dcabcfb0e6d618e014ab1812937fc
Macintosh HD <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-4.local>
2018-06-16 14:40:25 -0400

    first commit

---
+++ app/src/main/java/com/example/superdima/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,14 @@
+package com.example.superdima.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment(){
+        return NerdLauncherFragment.newInstance();
+    }
+
+
+}
